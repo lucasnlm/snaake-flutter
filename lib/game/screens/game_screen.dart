619f87d1d37bcb63f94d8714414cd39f8dbb9769
Lucas Lima <me@lucasnlm.dev>
2020-04-11 14:22:22 -0300

    Add gesture control

--- lib/game/screens/game_screen.dart
+++ lib/game/screens/game_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -65,23 +66,39 @@ class GameScreen extends StatelessWidget {
       appBar: appBar,
       extendBodyBehindAppBar: true,
       extendBody: true,
-      body: RawKeyboardListener(
-        focusNode: _focusNode,
-        onKey: (event) {
-          bloc.add(OnKeyPressedEvent(event.logicalKey));
+      body: GestureDetector(
+        onHorizontalDragUpdate: (details) {
+          if (details.delta.dx > 0.0) {
+            bloc.add(OnKeyPressedEvent(LogicalKeyboardKey.arrowRight));
+          } else {
+            bloc.add(OnKeyPressedEvent(LogicalKeyboardKey.arrowLeft));
+          }
         },
-        child: BlocListener<GameBloc, GameState>(
-          listener: (context, state) {
-            _gameRenderer.updateFood(state.food);
-            _gameRenderer.updateSnake(state.snake);
+        onVerticalDragUpdate: (details) {
+          if (details.delta.dy > 0.0) {
+            bloc.add(OnKeyPressedEvent(LogicalKeyboardKey.arrowDown));
+          } else {
+            bloc.add(OnKeyPressedEvent(LogicalKeyboardKey.arrowUp));
+          }
+        },
+        child: RawKeyboardListener(
+          focusNode: _focusNode,
+          onKey: (event) {
+            bloc.add(OnKeyPressedEvent(event.logicalKey));
           },
-          child: BlocBuilder<GameBloc, GameState>(
-            condition: (before, after) => before.status != Status.loading,
-            builder: (context, state) {
-              return state.status == Status.loading
-                  ? Loading()
-                  : _gameRenderer.widget;
+          child: BlocListener<GameBloc, GameState>(
+            listener: (context, state) {
+              _gameRenderer.updateFood(state.food);
+              _gameRenderer.updateSnake(state.snake);
             },
+            child: BlocBuilder<GameBloc, GameState>(
+              condition: (before, after) => before.status != Status.loading,
+              builder: (context, state) {
+                return state.status == Status.loading
+                    ? Loading()
+                    : _gameRenderer.widget;
+              },
+            ),
           ),
         ),
       ),
